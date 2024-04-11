af0230d89cb50b86141ffcc163a65e7a0a88f582
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-04-11 10:42:34 +0200

    DatePipe (in Deutsch)

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
 
 import { routes } from './app.routes';
 
+registerLocaleData(localeDe);
+
+
+console.log(localeDe)
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'de' }
+  ]
 };
--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { Book } from '../shared/book';
-import { JsonPipe, UpperCasePipe } from '@angular/common';
+import { DatePipe, JsonPipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [JsonPipe, UpperCasePipe],
+  imports: [JsonPipe, UpperCasePipe, DatePipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
+  jetzt = new Date();
+
   book: Book[] = [{
     isbn: '000',
     title: 'Angular',
-    description: 'Tolles Buch',
+    description: 'Tolles Buch!',
     rating: 5
   }, {
     isbn: '111',
