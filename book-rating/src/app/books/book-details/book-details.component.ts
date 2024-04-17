782382ecfd7197bfb861dec44348ab0578c79def
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-04-17 14:47:13 +0200

    rxjs: concatMap

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { concatMap, map, mergeMap, of } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { BookStoreService } from '../shared/book-store.service';
+
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, JsonPipe, RouterLink],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
 export class BookDetailsComponent {
+
+  bs = inject(BookStoreService);
+
+  book$ = toSignal(
+    inject(ActivatedRoute).paramMap.pipe(
+      map(paraMap => paraMap.get('isbn') || ''),
+      concatMap(isbn => this.bs.getSingleBook(isbn))
+    )
+  )
 
 }
